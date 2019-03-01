883e061cecb144f909aa29d89ddceed8df284dbe
weixiangxixi <30453074+weixiangxixi@users.noreply.github.com>
2019-03-01 15:17:12 +0800

    Update database.inc.php

--- system/config/database.inc.php
+++ system/config/database.inc.php
@@ -6,13 +6,13 @@ return array(
 
 	'default' => array (
 
-		'hostname' => 'rm-wz9c3uxpszo24y8ui.mysql.rds.aliyuncs.com', //数据库地址
+		'hostname' => '****', //数据库地址
 
-		'database' => 'tf123456789', //数据库名
+		'database' => '****', //数据库名
 
-		'username' => 'hxg_net', //数据库账户
+		'username' => '****', //数据库账户
 
-		'password' => '2342@fjjRREe8',      //数据库密码
+		'password' => '****',      //数据库密码
 
 		'tablepre' => 'go_',
 
